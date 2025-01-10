0dca22c6de934f8f8d875af076db4bb340e11ceb
teebabs521 <teebabs52@gmail.com>
2025-01-10 16:26:38 +0100

    Updates

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+exec { 'kill_killmenow_process':
+  command => '/usr/bin/pkill killmenow',
+  path    => ['/usr/bin', '/bin'],
+  onlyif  => '/usr/bin/pgrep killmenow', # Ensures the command runs only if the process exists
+}
+
